fc484f8217a23e4c9e049973a9a984dcae0e5064
MadhuKumarGowda <madhukumar.gowda@gmail.com>
2025-02-14 15:54:16 -0500

    created multiple components, user components defined using template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
+import { LoginComponent } from './components/login/login.component';
+import { UsersComponent } from './components/users/users.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AdminComponent, LoginComponent, UsersComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular19-sample';
---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  imports: [],
+  template: `<h1 class="text-xl text-purple-800 text-center">
+    Hello from User Page
+  </h1>`,
+})
+export class UsersComponent {}
