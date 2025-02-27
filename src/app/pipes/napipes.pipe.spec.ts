04bdf81db4ceb223ac878d2f8cc4c2fc0357aea2
MadhuKumarGowda <madhukumar.gowda@gmail.com>
2025-02-27 11:56:09 -0500

    Pipes implementation

---
+++ src/app/pipes/napipes.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { NAPipesPipe } from './napipes.pipe';
+
+describe('NAPipesPipe', () => {
+  it('create an instance', () => {
+    const pipe = new NAPipesPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
